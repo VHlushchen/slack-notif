9f23cda740e8097e3ece75e14ad37d9af2d6abc8
Vladyslav Hlushchenko <vhlushchenko@datagrok.ai>
2024-05-07 12:32:33 +0300

    logs

--- datagrok-install-kubernetes.sh
+++ datagrok-install-kubernetes.sh
@@ -457,7 +457,7 @@ function deploy_helm {
     
     start_time_running_state=$(date +%s)
     timeout_running_state=600
-    sleep 120
+    sleep 100
     echo ""
     while check_any_pod_not_running $namespace; do
         current_time=$(date +%s)
@@ -468,10 +468,10 @@ function deploy_helm {
         fi
         message "Not all pods are running. Waiting..."
         if [[ $verbose == true ]]; then
-            kubectl get pods -n $namespace
-            
             kubectl logs deployment/datagrok-1-18-4 -c datagrok -n datagrok-1-18-4
             kubectl logs deployment/init-wait-for-db -c datagrok -n datagrok-1-18-4
+            kubectl get pods -n $namespace
+            
         fi
         sleep 30  # Adjust the delay as needed
     done
